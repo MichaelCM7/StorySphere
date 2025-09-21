5de824acde3f22d693c1d7290cae6f54576c2c75
Mey14635 <kasomediatrice1407@gmail.com>
2025-09-21 13:57:28 +0300

    Update the page loading

--- Components/generalComponents.php
+++ Components/generalComponents.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <title><?php echo "StorySphere - Sign In" ?></title>
+     <title><?php echo "StorySphere - Sign forms" ?></title>
     <link rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
             crossorigin="anonymous">
@@ -12,15 +12,19 @@
 
 <?php
 class generalComponents {
-    public function formArea($form_components) {
+    public function formArea($form_components,$type='signIn') {
         ?>
         <div class="container my-5">
             <div class="row justify-content-center">
                 <div class="col-md-6">
                     <div class="card shadow-sm p-4">
                         <?php 
-                            $form_components->signInForm();
-                            $form_components->signUpForm();
+                             if ($type === 'signin') {
+                               $form_components->signInForm();
+                            }
+                             else {
+                             $form_components->signUpForm();
+                        }
                         ?>
                     </div>
                 </div>
