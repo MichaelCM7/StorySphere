e0dcd19e620d6bd4cad66dfb08391c631948375f
Mey14635 <kasomediatrice1407@gmail.com>
2025-09-21 13:02:44 +0300

    Create the signup form

--- Components/formComponents.php
+++ Components/formComponents.php
@@ -18,7 +18,34 @@ class formComponents {
 
             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
         </form>
-        <?php
-    }
+    <?php }
+    public function signUpForm() { ?>
+        <form method="post" action="signup_submit.php">
+            <h2 class="text-center mb-4">Sign Up</h2>
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" class="form-control" id="username" name="username" maxlength="30" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email Address:</label>
+                <input type="email" class="form-control" id="email" name="email" maxlength="50" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" id="password" name="password" maxlength="8" required>
+            </div>
+
+            <div class="form-group">
+                <label for="confirm_password">Confirm Password:</label>
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password" maxlength="8" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+            <a href="signIn.php">Already have an account? Sign in</a>
+        </form>
+        
+    <?php }
 }
 ?>
