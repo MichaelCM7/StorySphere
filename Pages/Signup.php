66ea59a54c2173dba3b8acb59601a8c5e9872c87
Mey14635 <kasomediatrice1407@gmail.com>
2025-09-21 13:58:22 +0300

    Try to load signUp 1

--- Pages/Signup.php
+++ Pages/Signup.php
@@ -4,6 +4,7 @@ require '../Components/formComponents.php';
 
 $general_components = new generalComponents();
 $form_components   = new formComponents();
+$type='signUp';
 
 $general_components->formArea($form_components);
 ?>
