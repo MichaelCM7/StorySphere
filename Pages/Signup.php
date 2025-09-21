ec458f1dd100d4f677b21261219529e11293cc5d
Mey14635 <kasomediatrice1407@gmail.com>
2025-09-21 13:19:10 +0300

    Create Object for the SignUp

---
+++ Pages/Signup.php
@@ -0,0 +1,9 @@
+<?php
+require '../Components/generalComponents.php';
+require '../Components/formComponents.php';
+
+$general_components = new generalComponents();
+$form_components   = new formComponents();
+
+$general_components->formArea($form_components);
+?>
