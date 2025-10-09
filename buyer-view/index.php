37bae137cc9d971fa4fdc195e3f731f2a1625024
tums2060 <tumainiwamukota@gmail.com>
2025-10-09 22:53:52 +0300

    Fixed css linking to apply styling

--- buyer-view/index.php
+++ buyer-view/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Dashboard | LibraryHub</title>
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
   <div class="container">
