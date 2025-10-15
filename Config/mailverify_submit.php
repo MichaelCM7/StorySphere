b8809c27862d20a693da8427bcb62a31864e53fb
Michael Mbugua <michaelmbugua777@gmail.com>
2025-10-15 11:02:43 +0300

    redirect to admin dash

--- Config/mailverify_submit.php
+++ Config/mailverify_submit.php
@@ -8,6 +8,7 @@ if (isset($_SESSION['otp'], $_SESSION['otp_created']) &&
     (time() - $_SESSION['otp_created'] <= $timeout)) {
       if (trim((string)$enteredCode) === trim((string)$_SESSION['otp'])) {
           echo "Your email has been verified";
+          header("Location: ../Pages/admin_dashboard.php");
           unset($_SESSION['otp'], $_SESSION['otp_created']);
       } else {
           echo "Wrong entry";
