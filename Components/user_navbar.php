5afa761e2ab5773f2587e570d4e2730af53c4b6d
tums2060 <tumainiwamukota@gmail.com>
2025-10-15 16:33:10 +0300

    Fixed linking in user_navbar.php

--- Components/user_navbar.php
+++ Components/user_navbar.php
@@ -17,11 +17,11 @@
   </div>
 
   <ul class="nav-links">
-    <li><a href="/Pages/user_index_dashboard.php"><i class="fa-solid fa-gauge"></i> Dashboard</a></li>
-    <li><a href="/Pages/user_browse_books.php"><i class="fa-solid fa-book-open"></i> Browse Books</a></li>
-    <li><a href="/Pages/user_borrowed_books.php"><i class="fa-solid fa-book-reader"></i> Borrowed Books</a></li>
-    <li><a href="/Pages/user_fines.php"><i class="fa-solid fa-coins"></i> Fines</a></li>
-    <li><a href="/Pages/user_profile.php"><i class="fa-solid fa-user"></i> Profile</a></li>
+    <li><a href="../Pages/user_index_dashboard.php"><i class="fa-solid fa-gauge"></i> Dashboard</a></li>
+    <li><a href="../Pages/user_browse_books.php"><i class="fa-solid fa-book-open"></i> Browse Books</a></li>
+    <li><a href="../Pages/user_borrowed_books.php"><i class="fa-solid fa-book-reader"></i> Borrowed Books</a></li>
+    <li><a href="../Pages/user_fines.php"><i class="fa-solid fa-coins"></i> Fines</a></li>
+    <li><a href="../Pages/user_profile.php"><i class="fa-solid fa-user"></i> Profile</a></li>
   </ul>
 
   <div class="logout">
